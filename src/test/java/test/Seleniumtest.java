0e7499742acf8e6e341f2c53a068459d8ebe756c
divya24692 <127322100+divya24692@users.noreply.github.com>
2023-03-08 20:15:35 +0530

    Initial Commit

---
+++ src/test/java/test/Seleniumtest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Seleniumtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://www.google.com/");
+	}
+
+}
